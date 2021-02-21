573ab721c78f0a97bdeedba263286d977f403769
Landon Schlangen <landonschlangen@Landons-MBP.hitronhub.home>
2021-02-20 21:44:19 -0600

    update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // This is where we fetch the data from newsapi.org
+    // one we have it, via the fetch method.
+    // we put the data into data using setData(newsarticles)
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{data}</header>
     </div>
   );
 }
